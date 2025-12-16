09e0653c4238040970b706c20053b3e66df9b4a8
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-12-17 00:07:10 +0530

    changes for glitter text

--- components/TextGlitterAnimation.tsx
+++ components/TextGlitterAnimation.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import "./textGlitter.css";
 
 const TextGlitterAnimation = () => {
   return (
-    <div className="flex flex-col justify-center">
-      <p>i am batman</p>
-      <p>kuch toh crazy kr dunga</p>
-      <p>i am gonna show you how great i am</p>
+    <div className="flex flex-col justify-center ">
+      <p className="highlight-text mb-2">
+        i am batman xdcfg ghjk yuiop ghjkbnm
+      </p>
+      <p className="highlight-text mb-2">
+        kuch toh crazy kr dunga xdcfg ghjk yuiop dfghyuiop
+      </p>
+      <p className="highlight-text">
+        i am gonna show you how great i am xdcfg ghjk yuiop dfghy
+      </p>
     </div>
   );
 };
