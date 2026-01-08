6317cdd34ea780df537fc266dd26812f9b6ebb63
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-01-08 20:27:27 +0530

    Final touches for link sharing card component

--- components/LinkSharingCard.tsx
+++ components/LinkSharingCard.tsx
@@ -8,6 +8,7 @@ import { GoPersonAdd } from "react-icons/go";
 import { MdDeleteOutline } from "react-icons/md";
 import { LuCopy } from "react-icons/lu";
 import { FaRegCheckCircle } from "react-icons/fa";
+import { IoEarthOutline } from "react-icons/io5";
 
 const LinkSharingCard = () => {
   return (
@@ -19,13 +20,13 @@ const LinkSharingCard = () => {
         <div className="flex items-center justify-center">
           <div className="mr-4 flex items-center justify-center border-b py-2">
             <span className="mr-2">
-              <HiLockClosed />
+              <HiLockClosed size={14} />
             </span>
             <span>Share Link</span>
           </div>
           <div className="flex items-center justify-center">
             <span className="mr-2">
-              <BsGlobeEuropeAfrica />
+              <IoEarthOutline size={14} />
             </span>
             <span>Publish Online</span>
           </div>
@@ -39,17 +40,18 @@ const LinkSharingCard = () => {
       </div>
       <div className="flex items-center justify-center">
         <div className="w-[92%] h-14 border border-neutral-300 rounded-lg flex items-center justify-between px-2.5 py-2.5">
-          <div>
-            <div className="w-8 h-8 rounded bg-[url('https://images.unsplash.com/photo-1710184713246-91865a6123dc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMxfHxncmFkaWVudCUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D')] bg-cover flex items-center justify-center">
+          <div className="flex items-center justify-center">
+            <div className="w-8 h-8 rounded bg-[url('https://images.unsplash.com/photo-1710184713246-91865a6123dc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMxfHxncmFkaWVudCUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D')] bg-cover flex items-center justify-center mr-3">
               <RiFileListFill className="text-white" />
             </div>
+            <div className="flex flex-col justify-between h-full">
+              <p className="text-xs">2027 Funding Rounds Update: Indicators</p>
+              <p className="text-[10px] text-neutral-500">
+                Text documents Created on 4:20 PM, September 9
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col justify-between h-full">
-            <p className="text-xs">2027 Funding Rounds Update: Indicators</p>
-            <p className="text-[10px] text-neutral-500">
-              Text documents Created on 4:20 PM, September 9
-            </p>
-          </div>
+
           <div className="text-black cursor-pointer">
             <CiLock />
           </div>
@@ -64,14 +66,15 @@ const LinkSharingCard = () => {
       </div>
       <div className="flex items-center justify-center">
         <div className="w-[92%] h-14 border border-neutral-300 rounded-lg flex items-center justify-between px-2.5 py-2.5">
-          <div>
-            <div className="w-7 h-7 rounded-lg bg-neutral-100 bg-cover flex items-center justify-center cursor-pointer">
+          <div className="flex items-center justify-center">
+            <div className="w-7 h-7 rounded-lg bg-neutral-100 bg-cover flex items-center justify-center cursor-pointer mr-3">
               <GoPersonAdd size={14} />
             </div>
+            <div className="flex flex-col justify-center h-full">
+              <p className="text-sm">Registered users with private link</p>
+            </div>
           </div>
-          <div className="flex flex-col justify-center h-full">
-            <p className="text-sm">Registered users with private link</p>
-          </div>
+
           <div className="text-black cursor-pointer">
             <p className="text-xs border border-neutral-300 rounded-md px-2.5 py-0.5">
               view
@@ -139,20 +142,20 @@ const LinkSharingCard = () => {
       <div className="border-t px-3.5 h-9.5 mt-4 border-neutral-300 flex items-center justify-between">
         <div className="flex items-center justify-center border border-neutral-300 px-2 py-0.5 rounded-md cursor-pointer">
           <span>
-            <BsGlobeEuropeAfrica size={13} className="mr-1.5" />
+            <IoEarthOutline size={12} className="mr-1.5" />
           </span>
           <span className="text-xs">Make Public</span>
         </div>
         <div className="flex items-center justify-center">
           <div className="flex items-center justify-center border border-neutral-300 px-2 py-0.5 rounded-md cursor-pointer mr-2">
             <span>
-              <LuCopy size={13} className="mr-1.5" />
+              <LuCopy size={12} className="mr-1.5" />
             </span>
             <span className="text-xs">Copy Link</span>
           </div>
           <div className="flex items-center justify-center border border-neutral-300 px-2 py-0.5 rounded-md cursor-pointer">
             <span>
-              <FaRegCheckCircle size={13} className="mr-1.5" />
+              <FaRegCheckCircle size={12} className="mr-1.5" />
             </span>
             <span className="text-xs">Done</span>
           </div>
