f0e91a37fd4051e85d760fbb2b43a22c0464f73a
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-01-10 13:05:38 +0530

    icon alignment in notch bar animation component

--- components/NotchBarAnimation.tsx
+++ components/NotchBarAnimation.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { FaSpotify } from "react-icons/fa";
+import { FaClock } from "react-icons/fa6";
+import { IoWifi } from "react-icons/io5";
+import { FaBatteryFull } from "react-icons/fa6";
+import { FaCloud } from "react-icons/fa";
 
 const NotchBarAnimation = () => {
   return (
@@ -6,7 +10,22 @@ const NotchBarAnimation = () => {
       style={{ fontFamily: "var(--font-geist-sans)" }}
       className="w-150 h-100 border border-neutral-300 rounded-lg"
     >
-      <div className="w-full h-[15%] bg-black mt-18"></div>
+      <div className="w-full h-[15%] bg-black mt-18 flex items-center justify-center">
+        <FaSpotify
+          className="text-neutral-200 hover:text-white mr-6"
+          size={19}
+        />
+        <FaClock className="text-neutral-200 hover:text-white mr-6" size={18} />
+        <IoWifi className="text-neutral-200 hover:text-white mr-6" size={22} />
+        <FaBatteryFull
+          className="text-neutral-200 hover:text-white mr-6"
+          size={22}
+        />
+        <span className="flex items-center justify-center text-neutral-200 hover:text-white">
+          <FaCloud className="mr-1.5" size={20} />
+          <p className="cursor-default text-xs">80°F</p>
+        </span>
+      </div>
     </div>
   );
 };
