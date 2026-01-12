de612747a6d311bac135298c51a66abfa4be8020
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-01-13 00:23:29 +0530

    changes for layout in ios side notch component

--- components/IosSideNotch.tsx
+++ components/IosSideNotch.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { IoWifi } from "react-icons/io5";
+import { IoSearchSharp } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { IoIosBatteryDead } from "react-icons/io";
 
 const IosSideNotch = () => {
   return (
-    <div style={{ fontFamily: "var(--font-geist-sans)" }}>
-      <p>notch</p>
+    <div
+      style={{ fontFamily: "var(--font-geist-sans)" }}
+      className="w-130 h-90 border border-neutral-300 flex items-end bg-[#e9e9e9]"
+    >
+      <div className="w-115 h-75 border border-neutral-300 rounded-tr-3xl bg-black overflow-y-hidden">
+        <div className="w-full h-10 flex items-end justify-end pr-10 text-white pb-1">
+          <div className="flex items-center justify-center">
+            <IoWifi className="mr-3" />
+            <IoSearchSharp className="mr-3" />
+            <CgProfile className="mr-3" />
+            {/* <IoIosBatteryDead /> */}
+            <p className="text-white text-xs">Mon Jun 5 9:41 AM</p>
+          </div>
+        </div>
+        <div className="h-full w-111 bg-blue-300"></div>
+      </div>
     </div>
   );
 };
