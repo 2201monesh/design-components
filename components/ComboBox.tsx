600f4d72eb753739bf7585b37b853c1a4ca09b8c
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-01-16 20:05:57 +0530

    changes for combobox main div

--- components/ComboBox.tsx
+++ components/ComboBox.tsx
@@ -4,13 +4,19 @@ const ComboBox = () => {
   return (
     <div
       style={{ fontFamily: "var(--font-geist-sans)" }}
-      className="flex items-center justify-center w-full h-full bg-[#f6f6f6]"
+      className="flex items-start justify-center w-full h-full bg-[#f6f6f6]"
     >
-      <input
-        type="text"
-        className="outline-neutral-300 w-[25%] px-4 py-2 shadow rounded bg-white"
-        placeholder="Search companies..."
-      />
+      <div className="flex flex-col items-center justify-center w-full h-full border">
+        <input
+          type="text"
+          className="outline-neutral-300 w-[25%] px-4 py-2 shadow-sm rounded-lg bg-white mb-1.5"
+          placeholder="Search companies..."
+        />
+        <div className="w-[25%] h-100 border rounded-lg border-neutral-200 bg-white shadow-sm">
+          <div className="w-full h-[88%] overflow-y-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"></div>
+          <div className="w-full h-[12%] bg-[#f6f6f6] rounded-b-lg border-t border-t-neutral-200"></div>
+        </div>
+      </div>
     </div>
   );
 };
